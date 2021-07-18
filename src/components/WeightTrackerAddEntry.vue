de019c08531847ce162a3384c11f2fb94fdcd777
holloway <me@holloway-web.de>
2021-07-18 15:24:59 +0200

    added basic weight tracker

---
+++ src/components/WeightTrackerAddEntry.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="weight-tracker-add-entry">
+    <CCard>
+      <CCardHeader>Add entry</CCardHeader>
+      <CCardBody>
+        <CInput label="Date" v-model="date" type="date" />
+        <CInput label="Weight" v-model="weight" />
+        <CButton color="primary" type="submit" v-on:click="onClick()">
+          Add
+        </CButton>
+      </CCardBody>
+    </CCard>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: "",
+      weight: null,
+    };
+  },
+  methods: {
+    /**
+     * Adds the entry to the weight tracker and resets the data.
+     */
+    onClick() {
+      if (!this.date || !this.weight) {
+        return;
+      }
+
+      this.addEntry(this.date, parseFloat(this.weight));
+      this.date = "";
+      this.weight = null;
+    },
+  },
+  props: ["addEntry"],
+};
+</script>
--- src/views/WeightTracker.vue
+++ src/views/WeightTracker.vue
@@ -1,7 +1,103 @@
 <template>
   <div class="weight-tracker">
-    <CCard>
-      <CCardHeader>Weight tracker</CCardHeader>
-    </CCard>
+    <CRow>
+      <CCol sm="8" lg="9">
+        <CCard>
+          <CCardHeader>Weight tracker</CCardHeader>
+          <CCardBody>
+            <CChartLine
+              :datasets="weightDatasets"
+              :labels="weightDatasetLabels"
+            />
+          </CCardBody>
+        </CCard>
+      </CCol>
+      <CCol sm="4" lg="3">
+        <WeightTrackerAddEntry :addEntry="addEntry" />
+      </CCol>
+    </CRow>
   </div>
 </template>
+
+<!--suppress JSUnfilteredForInLoop -->
+<script>
+import { CChartLine } from "@coreui/vue-chartjs";
+import WeightTrackerAddEntry from "../components/WeightTrackerAddEntry";
+
+export default {
+  components: { CChartLine, WeightTrackerAddEntry },
+  data() {
+    return {
+      weightData: this.loadWeightData(),
+      weightDatasets: [],
+      weightDatasetLabels: [],
+    };
+  },
+  methods: {
+    /**
+     * Add a weight entry for a given date.
+     */
+    addEntry(date, weight) {
+      this.weightData[date] = weight;
+      this.calcWeightDatasets();
+      this.saveWeightData();
+    },
+
+    /**
+     * Updates the data for the line chart.
+     */
+    calcWeightDatasets() {
+      let data = [];
+      let labels = [];
+      for (let e in this.weightData) {
+        labels.push(
+          Intl.DateTimeFormat(navigator.language, {
+            day: "2-digit",
+            month: "2-digit",
+          }).format(new Date(e))
+        );
+        data.push(this.weightData[e]);
+      }
+
+      this.weightDatasets = [
+        {
+          backgroundColor: "#a0f0a0",
+          data: data,
+          label: "Weight",
+        },
+      ];
+      this.weightDatasetLabels = labels;
+    },
+
+    /**
+     * Loads the weight data from local storage.
+     */
+    loadWeightData() {
+      let data = localStorage.getItem("weight-data");
+      if (null === data) {
+        data = "";
+      }
+
+      try {
+        return JSON.parse(data);
+      } catch (e) {
+        return {};
+      }
+    },
+
+    /**
+     * Saves the weight data in local storage.
+     */
+    saveWeightData() {
+      localStorage.setItem("weight-data", JSON.stringify(this.weightData));
+    },
+  },
+
+  /**
+   * Calculates the chart data when mounted.
+   */
+  mounted() {
+    this.calcWeightDatasets();
+  },
+};
+</script>
